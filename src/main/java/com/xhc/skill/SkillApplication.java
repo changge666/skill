d4b34f22b1619f86284d46b967034a6c40e8371a
changgge666 <2628996289@qq.com>
2022-07-13 21:25:28 +0800

    提交文件

---
+++ src/main/java/com/xhc/skill/SkillApplication.java
@@ -0,0 +1,13 @@
+package com.xhc.skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xhc/skill/SkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhc.skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
